438298df1e3ddde392f3f98ef7714a0ebfb62e0c
Jessica9-star <jessica@operatorfoundation.org>
2020-07-29 13:15:33 -0500

    Added Buttons

--- app/src/main/java/org/operatorfoundation/nahoft/MainActivity.kt
+++ app/src/main/java/org/operatorfoundation/nahoft/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.operatorfoundation.nahoft
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        messages_button.setOnClickListener {
+            println("message button clicked")
+            val intent = Intent(this, MessagesActivity::class.java)
+            startActivity(intent)
+        }
+        help_button.setOnClickListener {
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
+        }
+        keys_button.setOnClickListener {
+            val intent = Intent(this, KeysActivity::class.java)
+            startActivity(intent)
+        }
+        settings_button.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
