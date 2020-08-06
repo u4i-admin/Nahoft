2a66749226d56f3c8d304f98ce7c6063180a96fc
Jessica9-star <jessica@operatorfoundation.org>
2020-08-06 10:21:27 -0500

    Update send_as_text_button

--- app/src/main/java/org/operatorfoundation/nahoft/NewMessageActivity.kt
+++ app/src/main/java/org/operatorfoundation/nahoft/NewMessageActivity.kt
@@ -1,11 +1,23 @@
 package org.operatorfoundation.nahoft
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+
+        send_as_text_button.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
     }
 }
