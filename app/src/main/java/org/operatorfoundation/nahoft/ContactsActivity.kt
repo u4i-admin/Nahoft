bab176738c206630b3fad3bf675e1a77561d541f
Jessica9-star <jessica@operatorfoundation.org>
2020-08-12 18:50:36 -0500

    Send As Text Button Updated

--- app/src/main/java/org/operatorfoundation/nahoft/ContactsActivity.kt
+++ app/src/main/java/org/operatorfoundation/nahoft/ContactsActivity.kt
@@ -7,11 +7,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.database.Cursor
 import androidx.loader.app.LoaderManager.LoaderCallbacks
 import android.content.Intent
+import android.net.Uri
 import android.os.Parcel
 import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.provider.ContactsContract.Contacts
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CursorAdapter
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 
+val from_columns : Array<String> = arrayOf(Contacts.DISPLAY_NAME_PRIMARY)
+
+private val TO_IDS: IntArray = intArrayOf(android.R.id.text1)
 
 class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +34,95 @@ class ContactsActivity : AppCompatActivity() {
 
 abstract class ContactsFragment() : Fragment(), LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener,
     Parcelable {
+    // Define global mutable variables
+    // Define a ListView object
+    lateinit var contactsList: ListView
+    // Define variables for the contact the user selects
+    // The contact's _ID value
+    var contactId: Long = 0
+    // The contact's LOOKUP_KEY
+    var contactKey: String? = null
+    // A content URI for the selected contact
+    var contactUri: Uri? = null
+    // An adapter that binds the result Cursor to the ListView
+    private var cursorAdapter: SimpleCursorAdapter? = null
+    val projection:Array<String> = arrayOf(Contacts._ID,
+        Contacts.LOOKUP_KEY,Contacts.DISPLAY_NAME)
+
+    val selectionArgs = arrayOf<String>("${Contacts.DISPLAY_NAME_PRIMARY} LIKE ?")
+
     constructor(parcel: Parcel) : this() {
+    }
+
+    override fun onItemClick(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        // Get the Cursor
+        val cursor: Cursor? = (parent.adapter as? CursorAdapter)?.cursor?.apply {
+            // Move to the selected contact
+            moveToPosition(position)
+            // Get the _ID value
+            contactId = getLong(0)
+            // Get the selected LOOKUP KEY
+            contactKey = getString(1)
+            // Create the contact's content Uri
+            contactUri = Contacts.getLookupUri(contactId, contactKey)
+            /*
+             * You can use contactUri as the content URI for retrieving
+             * the details for a contact.
+             */
+        }
+    }
+
+    // A UI Fragment must inflate its View
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the fragment layout
+        return inflater.inflate(R.layout.contact_list_fragment, container, false)
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // Gets the ListView from the View list of the parent activity
+        activity?.also {
+            contactsList = it.findViewById<ListView>(R.id.contact_list)
+            // Gets a CursorAdapter
+            cursorAdapter = SimpleCursorAdapter(
+                it,
+                R.layout.contacts_list_item,
+                null,
+                from_columns, TO_IDS,
+                0
+            )
+            // Sets the adapter for the ListView
+            contactsList.adapter = cursorAdapter
+        }
+        contactsList.onItemClickListener = this
+
+
+
+        loaderManager.initLoader(0, null, this)
+    }
+
+    override fun onCreateLoader(loaderId: Int, args: Bundle?): Loader<Cursor> {
+        /*
+         * Makes search string into pattern and
+         * stores it in the selection array
+         */
+        //FIXME find out what this search sting is supposed to be
+        selectionArgs[0] = "%mSearchString%"
+        // Starts the query
+        return activity?.let {
+            return CursorLoader(
+                it,
+                Contacts.CONTENT_URI,
+                projection,
+                "${Contacts.DISPLAY_NAME_PRIMARY} LIKE ?",
+                selectionArgs,
+                null
+            )
+        } ?: throw IllegalStateException()
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
--- app/src/main/java/org/operatorfoundation/nahoft/NewMessageActivity.kt
+++ app/src/main/java/org/operatorfoundation/nahoft/NewMessageActivity.kt
@@ -1,23 +1,115 @@
 package org.operatorfoundation.nahoft
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.ContentProvider
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
+//EditText message_text_view
+//Button send_as_text_button
+    val IMAGE_PICK_CODE = 1046
+    val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
 
+
+        friend_button.setOnClickListener {
+            println("friend button clicked")
+            loadContacts()
+        }
+
         send_as_text_button.setOnClickListener {
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                putExtra(Intent.EXTRA_TEXT, editMessageText.text)
                 type = "text/plain"
             }
             val shareIntent = Intent.createChooser(sendIntent, null)
             startActivity(shareIntent)
+        }
+
+        send_as_image_button.setOnClickListener {
+            pickImageFromGallery()
+        }
+    }
+
+    fun pickImageFromGallery() {
+        val pickImageIntent = Intent().apply {
+            action = Intent.ACTION_PICK
+            type = "image/*"
+        }
+        startActivityForResult(pickImageIntent, IMAGE_PICK_CODE)
+    }
+    fun loadContacts(){
+        var builder = StringBuilder()
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), PERMISSIONS_REQUEST_READ_CONTACTS)
+        } else {
+            builder = getContacts()
+            val contactsText = builder.toString()
+        }
+    }
+
+    fun getContacts(): StringBuilder {
+        val resolver: ContentResolver = contentResolver;
+        val builder = StringBuilder()
+        val cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI,null, null, null)
+
+        if (cursor != null) {
+            if (cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                    val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    println("found contact")
+                    println(id)
+                    println(name)
+                    builder.append("Contact ID: ").append(id).append("Name: ").append(name)
+                }
+            }
+        } else {
+            println("cursor is null")
+        }
+
+        return builder
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            val shareIntent: Intent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM, data?.data)
+                type = "image/jpeg"
+            }
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadContacts()
+            } else {
+                println("No Permission For Contacts")
+            }
         }
     }
 }
