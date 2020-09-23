4622d6969e41bcd65bb741ed8a9aa24a037c4896
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-09-23 18:21:04 -0500

    Update ShareUtil.kt

--- app/src/main/java/org/org/util/ShareUtil.kt
+++ app/src/main/java/org/org/util/ShareUtil.kt
@@ -1,10 +1,12 @@
 package org.org.util
 
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import org.org.codex.Codex
 import org.org.codex.Encryption
+import org.org.stencil.CapturePhotoUtils
 import org.org.stencil.Stencil
 
 
@@ -13,30 +15,38 @@ object ShareUtil {
     fun shareImage(context: Context, imageUri: Uri, message: String, encodedFriendPublicKey: ByteArray) {
 
         // Encrypt the message
-        // TODO: Use encrypted message in stencil
         val encryptedMessage = Encryption(context).encrypt(encodedFriendPublicKey, message)
 
-        // Encode the image
-        val newBitmap = Stencil().encode(context, encryptedMessage!!, imageUri)
+        if (encryptedMessage != null) {
+            // Encode the image
+            val newUri = Stencil().encode(context, encryptedMessage, imageUri)
 
-        // TODO: Save bitmap to image roll to get URI for sharing intent
+            // Save bitmap to image roll to get URI for sharing intent
+            if (newUri != null) {
 
-        // Sharing requires a custom intent whose action must be Intent.ACTION_SEND
-        val intent = Intent(Intent.ACTION_SEND).apply {
+                // Sharing requires a custom intent whose action must be Intent.ACTION_SEND
+                val intent = Intent(Intent.ACTION_SEND).apply {
 
-            // MIME type is always image/jpeg because the repo saves all the memes locally as JPEGs.
-            type = "image/jpeg"
+                    type = "image/jpeg"
 
-            // TODO: Share the new bitmap instead
-            // Resolves the meme's local URL and adds it to the intent as Intent.EXTRA_STREAM
-            putExtra(Intent.EXTRA_STREAM, imageUri)
+                    // Resolves the images local URL and adds it to the intent as Intent.EXTRA_STREAM
+                    putExtra(Intent.EXTRA_STREAM, newUri)
 
-            // You need this flag to let the intent read local data and stream the image content to another app.
-            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    // You need this flag to let the intent read local data and stream the image content to another app.
+                    flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                }
+
+                context.startActivity(Intent.createChooser(intent, null))
+            } else {
+                print("Unable to send message as photo, we were unable to encode the selected image.")
+                return
+            }
+        } else {
+            // TODO: Toast
+            print("Unable to send message as photo, we were unable to encrypt the message.")
+            return
         }
 
-        // Starts the share sheet.
-        context.startActivity(Intent.createChooser(intent, null))
     }
 
     fun shareText(context: Context, message: String, encodedFriendPublicKey: ByteArray) {
@@ -57,4 +67,5 @@ object ShareUtil {
         }
 
     }
+
 }
