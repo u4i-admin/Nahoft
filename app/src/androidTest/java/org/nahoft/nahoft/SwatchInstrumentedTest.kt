3c139741099e1c7899d2ed32a1997fb2daa70a82
Bluesaxorcist <joshua@operatorfoundation.org>
2021-04-19 12:03:31 -0500

    edited some tests

--- app/src/androidTest/java/org/nahoft/nahoft/SwatchInstrumentedTest.kt
+++ app/src/androidTest/java/org/nahoft/nahoft/SwatchInstrumentedTest.kt
@@ -12,7 +12,7 @@ class SwatchInstrumentedTest {
     @Test
     fun swatchEncodeDecodeTest() {
         val swatch = Swatch()
-        val encrypted = byteArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+        val encrypted = intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
             28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41)
 //        val instrumentation = getInstrumentation()
 //        val context = instrumentation.context
@@ -21,9 +21,9 @@ class SwatchInstrumentedTest {
         val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
 //        val puppyPic = R.drawable.the_puppy
 //        val bitmap = BitmapFactory.decodeResource(Resources.getSystem(), puppyPic)
-        val encoded = swatch.encode(encrypted, bitmap)
+        val encoded = swatch.encode(bitmap, intArrayOf(encrypted.size), encrypted)
         var decoded = ByteArray(42)
-        if (encoded != null) decoded = swatch.decode(encoded)!!
+        if (encoded != null) decoded = swatch.decode(byteArrayOf(encoded), 0, 42)!!
         Assert.assertEquals(encrypted, decoded)
     }
 
