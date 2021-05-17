96fd689fdfc242136a5891d7fbe9a095a368573f
consuelita <adelita@operatorfoundation.org>
2021-05-17 12:46:45 -0500

    Strings cleanup

--- app/src/main/java/org/nahoft/util/SaveUtil.kt
+++ app/src/main/java/org/nahoft/util/SaveUtil.kt
@@ -7,8 +7,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
-import android.provider.Settings.Global.getString
-import org.nahoft.nahoft.R
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
