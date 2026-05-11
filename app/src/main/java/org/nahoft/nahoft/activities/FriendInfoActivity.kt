446bc8d13d9338dd7e830784926f054853840d61
consuelita <adelita@operatorfoundation.org>
2026-05-11 16:29:58 -0500

    Import Text Dialogue UI fix

--- app/src/main/java/org/nahoft/nahoft/activities/FriendInfoActivity.kt
+++ app/src/main/java/org/nahoft/nahoft/activities/FriendInfoActivity.kt
@@ -13,7 +13,6 @@ import android.os.Parcelable
 import android.text.Layout
 import android.text.SpannableString
 import android.text.style.AlignmentSpan
-import android.view.Gravity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
